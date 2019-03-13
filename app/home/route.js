636d77a0db6ec3de0b2407f409b34a97667f4ac4
Syed Shahiq <syed.shahiq@square63.com>
2019-03-13 19:31:20 +0500

    Api hit to usres

--- app/home/route.js
+++ app/home/route.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model: function(){
+       return ({
+           user :this.store.findRecord('user',1)
+       }); 
+    }
 });
