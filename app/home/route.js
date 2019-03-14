ed1369d393d32ac345d9306dd487e88aef7c4aaf
Syed Shahiq <syed.shahiq@square63.com>
2019-03-14 12:04:06 +0500

    Quotes list added

--- app/home/route.js
+++ app/home/route.js
@@ -3,7 +3,8 @@ import Route from '@ember/routing/route';
 export default Route.extend({
     model: function(){
        return ({
-           user :this.store.findRecord('user',1)
+           user :this.store.findRecord('user',1),
+           quotes :this.store.findAll('quote')
        }); 
     }
 });
---
+++ mirage/fixtures/quotes.js
@@ -0,0 +1,12 @@
+export default[
+    {
+        id: 1,
+        text:"Hi I am working on Ember js",
+        user:1
+    },
+    {
+        id: 2,
+        text:"Hi This is Shahiq and i loves to code.",
+        user:1
+    }
+];
